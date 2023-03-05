78c8ae3215e65f5442491e7e0d0986f5f17106cc
jennychiang <jennyc201911@gmail.com>
2023-03-05 15:00:45 +0800

    add: leetcode 78

--- 2023/78-subsets.js
+++ 2023/78-subsets.js
@@ -18,7 +18,7 @@ var subsets = function (nums) {
  * @returns {number[][]}
  */
 let go = function (nums, tempNums, currentIndex, result) {
-    result.push([...tempNums]); // 參照問題，複製一個 tempNums，再放入結果中，以免後續改了 tempNums，全部的結果一併被改掉。ㄋ
+    result.push([...tempNums]); // 參照問題，複製一個 tempNums，再放入結果中，以免後續改了 tempNums，全部的結果一併被改掉。
     for (let i = currentIndex; i < nums.length; i++) {
         tempNums.push(nums[i]);
         currentIndex++;
